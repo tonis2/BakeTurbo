f3f99399d27360ae7651e826aa1029e3e40a1e88
tonis2 <tonis.anton18@gmail.com>
2026-04-01 00:50:11 +0300

    Fixes for assign on path

--- trimsheet/trim_path.py
+++ trimsheet/trim_path.py
@@ -104,13 +104,17 @@ def _build_core(tree, curves_socket, group_in, group_out, x_off=0):
     link(group_in.outputs['Resolution'], resample.inputs['Count'])
 
     # --- Snap curve points to surface (before curve-to-mesh) ---
-    obj_info = _add_node(tree, 'GeometryNodeObjectInfo', (x + 200, -500))
+    obj_info = _add_node(tree, 'GeometryNodeObjectInfo', (x + 200, -600))
     obj_info.transform_space = 'RELATIVE'
     link(group_in.outputs['Surface Target'], obj_info.inputs['Object'])
 
     pos_curve = _add_node(tree, 'GeometryNodeInputPosition', (x + 200, -200))
-
-    # Sample nearest position on target surface
+    
+    # Check if target object is valid (has geometry)
+    is_valid_obj = _add_node(tree, 'GeometryNodeObjectInfo', (x + 200, -800))
+    link(group_in.outputs['Surface Target'], is_valid_obj.inputs['Object'])
+    
+    # Sample nearest position on target surface (with fallback)
     sample_pos = _add_node(tree, 'GeometryNodeSampleNearestSurface', (x + 400, -200))
     sample_pos.data_type = 'FLOAT_VECTOR'
     link(obj_info.outputs['Geometry'], sample_pos.inputs['Mesh'])
@@ -126,34 +130,74 @@ def _build_core(tree, curves_socket, group_in, group_out, x_off=0):
     nrm_tgt = _add_node(tree, 'GeometryNodeInputNormal', (x + 400, -650))
     link(nrm_tgt.outputs['Normal'], _value_socket(sample_nrm))
 
-    # Switch: snap or keep original curve positions
-    switch_pos = _add_node(tree, 'GeometryNodeSwitch', (x + 600, -200))
+    # Default normal (Z-up) when no surface target
+    default_nrm = _add_node(tree, 'FunctionNodeInputVector', (x + 400, -850))
+    default_nrm.vector = (0.0, 0.0, 1.0)
+    
+    # Switch between sampled normal and default based on whether we have a valid target
+    switch_nrm_fallback = _add_node(tree, 'GeometryNodeSwitch', (x + 600, -500))
+    switch_nrm_fallback.input_type = 'VECTOR'
+    link(is_valid_obj.outputs['Geometry'], switch_nrm_fallback.inputs['Switch'])
+    link(default_nrm.outputs['Vector'], switch_nrm_fallback.inputs['False'])
+    link(_value_socket(sample_nrm, is_output=True), switch_nrm_fallback.inputs['True'])
+    
+    # Switch position with fallback
+    switch_pos_fallback = _add_node(tree, 'GeometryNodeSwitch', (x + 600, -200))
+    switch_pos_fallback.input_type = 'VECTOR'
+    link(is_valid_obj.outputs['Geometry'], switch_pos_fallback.inputs['Switch'])
+    link(pos_curve.outputs['Position'], switch_pos_fallback.inputs['False'])
+    link(_value_socket(sample_pos, is_output=True), switch_pos_fallback.inputs['True'])
+
+    # Switch: snap or keep original curve positions (controlled by Snap to Surface toggle)
+    switch_pos = _add_node(tree, 'GeometryNodeSwitch', (x + 800, -200))
     switch_pos.input_type = 'VECTOR'
     link(group_in.outputs['Snap to Surface'], switch_pos.inputs['Switch'])
     link(pos_curve.outputs['Position'], switch_pos.inputs['False'])
-    link(_value_socket(sample_pos, is_output=True), switch_pos.inputs['True'])
+    link(switch_pos_fallback.outputs['Output'], switch_pos.inputs['True'])
 
     # Offset snapped position along surface normal
-    nrm_scale = _add_node(tree, 'ShaderNodeVectorMath', (x + 600, -400))
+    nrm_scale = _add_node(tree, 'ShaderNodeVectorMath', (x + 800, -400))
     nrm_scale.operation = 'SCALE'
-    link(_value_socket(sample_nrm, is_output=True), nrm_scale.inputs[0])
+    link(switch_nrm_fallback.outputs['Output'], nrm_scale.inputs[0])
     link(group_in.outputs['Surface Offset'], nrm_scale.inputs['Scale'])
 
     # Apply snap + offset to curve points
-    set_curve_pos = _add_node(tree, 'GeometryNodeSetPosition', (x + 800, 0))
+    set_curve_pos = _add_node(tree, 'GeometryNodeSetPosition', (x + 1000, 0))
     link(resample.outputs['Curve'], set_curve_pos.inputs['Geometry'])
     link(switch_pos.outputs['Output'], set_curve_pos.inputs['Position'])
     link(nrm_scale.outputs['Vector'], set_curve_pos.inputs['Offset'])
 
-    # Set curve normal to surface normal so profile lies flat on surface
-    set_curve_nrm = _add_node(tree, 'GeometryNodeSetCurveNormal', (x + 1000, 0))
-    set_curve_nrm.inputs['Mode'].default_value = 'Minimum Twist'
-    link(set_curve_pos.outputs['Geometry'], set_curve_nrm.inputs['Curve'])
+    # Store surface normal as a named attribute on curve points
+    store_nrm = _add_node(tree, 'GeometryNodeStoreNamedAttribute', (x + 1150, -100))
+    store_nrm.data_type = 'FLOAT_VECTOR'
+    store_nrm.domain = 'POINT'
+    store_nrm.inputs['Name'].default_value = "_bt_surface_normal"
+    link(set_curve_pos.outputs['Geometry'], store_nrm.inputs['Geometry'])
+    link(switch_nrm_fallback.outputs['Output'], _value_socket(store_nrm))
+
+    # Get curve tangent and compute cross product with surface normal
+    # This gives a vector perpendicular to both, lying flat on the surface
+    curve_tangent = _add_node(tree, 'GeometryNodeInputTangent', (x + 1250, -400))
+    read_nrm = _add_node(tree, 'GeometryNodeInputNamedAttribute', (x + 1250, -550))
+    read_nrm.data_type = 'FLOAT_VECTOR'
+    read_nrm.inputs['Name'].default_value = "_bt_surface_normal"
+    
+    # Cross product: tangent × surface_normal = flat normal on surface
+    cross_nrm = _add_node(tree, 'ShaderNodeVectorMath', (x + 1450, -400))
+    cross_nrm.operation = 'CROSS_PRODUCT'
+    link(curve_tangent.outputs['Tangent'], cross_nrm.inputs[0])
+    link(_value_socket(read_nrm, is_output=True), cross_nrm.inputs[1])
+
+    # Set curve normal to the cross product result so profile lies flat on surface
+    set_curve_nrm = _add_node(tree, 'GeometryNodeSetCurveNormal', (x + 1650, 0))
+    set_curve_nrm.inputs['Mode'].default_value = 'Free'
+    link(store_nrm.outputs['Geometry'], set_curve_nrm.inputs['Curve'])
+    link(cross_nrm.outputs['Vector'], set_curve_nrm.inputs['Normal'])
 
     # --- Store path factor ---
-    sp_path = _add_node(tree, 'GeometryNodeSplineParameter', (x + 1000, -200))
+    sp_path = _add_node(tree, 'GeometryNodeSplineParameter', (x + 1600, -200))
 
-    store_path = _add_node(tree, 'GeometryNodeStoreNamedAttribute', (x + 1200, 0))
+    store_path = _add_node(tree, 'GeometryNodeStoreNamedAttribute', (x + 1800, 0))
     store_path.data_type = 'FLOAT'
     store_path.domain = 'POINT'
     store_path.inputs['Name'].default_value = "_bt_path_factor"
@@ -161,25 +205,25 @@ def _build_core(tree, curves_socket, group_in, group_out, x_off=0):
     link(sp_path.outputs['Factor'], _value_socket(store_path))
 
     # --- Profile line scaled by Width ---
-    line = _add_node(tree, 'GeometryNodeCurvePrimitiveLine', (x + 1000, -500))
+    line = _add_node(tree, 'GeometryNodeCurvePrimitiveLine', (x + 1800, -500))
     line.mode = 'POINTS'
     line.inputs['Start'].default_value = (-0.5, 0.0, 0.0)
     line.inputs['End'].default_value = (0.5, 0.0, 0.0)
 
-    pos_in = _add_node(tree, 'GeometryNodeInputPosition', (x + 1000, -700))
-    vec_scale_w = _add_node(tree, 'ShaderNodeVectorMath', (x + 1200, -700))
+    pos_in = _add_node(tree, 'GeometryNodeInputPosition', (x + 1800, -700))
+    vec_scale_w = _add_node(tree, 'ShaderNodeVectorMath', (x + 2000, -700))
     vec_scale_w.operation = 'SCALE'
     link(pos_in.outputs['Position'], vec_scale_w.inputs[0])
     link(group_in.outputs['Width'], vec_scale_w.inputs['Scale'])
 
-    set_pos_prof = _add_node(tree, 'GeometryNodeSetPosition', (x + 1200, -500))
+    set_pos_prof = _add_node(tree, 'GeometryNodeSetPosition', (x + 2000, -500))
     link(line.outputs['Curve'], set_pos_prof.inputs['Geometry'])
     link(vec_scale_w.outputs['Vector'], set_pos_prof.inputs['Position'])
 
     # --- Store profile factor ---
-    sp_prof = _add_node(tree, 'GeometryNodeSplineParameter', (x + 1400, -700))
+    sp_prof = _add_node(tree, 'GeometryNodeSplineParameter', (x + 2200, -700))
 
-    store_prof = _add_node(tree, 'GeometryNodeStoreNamedAttribute', (x + 1400, -500))
+    store_prof = _add_node(tree, 'GeometryNodeStoreNamedAttribute', (x + 2200, -500))
     store_prof.data_type = 'FLOAT'
     store_prof.domain = 'POINT'
     store_prof.inputs['Name'].default_value = "_bt_profile_factor"
@@ -187,40 +231,40 @@ def _build_core(tree, curves_socket, group_in, group_out, x_off=0):
     link(sp_prof.outputs['Factor'], _value_socket(store_prof))
 
     # --- Curve to Mesh ---
-    c2m = _add_node(tree, 'GeometryNodeCurveToMesh', (x + 1600, 0))
+    c2m = _add_node(tree, 'GeometryNodeCurveToMesh', (x + 2400, 0))
     c2m.inputs['Fill Caps'].default_value = False
     link(store_path.outputs['Geometry'], c2m.inputs['Curve'])
     link(store_prof.outputs['Geometry'], c2m.inputs['Profile Curve'])
 
     # --- UV mapping: read factors, map to trim bounds ---
-    read_path = _add_node(tree, 'GeometryNodeInputNamedAttribute', (x + 1600, -200))
+    read_path = _add_node(tree, 'GeometryNodeInputNamedAttribute', (x + 2600, -200))
     read_path.data_type = 'FLOAT'
     read_path.inputs['Name'].default_value = "_bt_path_factor"
 
-    map_u = _add_node(tree, 'ShaderNodeMapRange', (x + 1800, -200))
+    map_u = _add_node(tree, 'ShaderNodeMapRange', (x + 2800, -200))
     link(_value_socket(read_path, is_output=True), map_u.inputs['Value'])
     map_u.inputs['From Min'].default_value = 0.0
     map_u.inputs['From Max'].default_value = 1.0
     link(group_in.outputs['U Min'], map_u.inputs['To Min'])
     link(group_in.outputs['U Max'], map_u.inputs['To Max'])
 
-    read_prof = _add_node(tree, 'GeometryNodeInputNamedAttribute', (x + 1600, -400))
+    read_prof = _add_node(tree, 'GeometryNodeInputNamedAttribute', (x + 2600, -400))
     read_prof.data_type = 'FLOAT'
     read_prof.inputs['Name'].default_value = "_bt_profile_factor"
 
-    map_v = _add_node(tree, 'ShaderNodeMapRange', (x + 1800, -400))
+    map_v = _add_node(tree, 'ShaderNodeMapRange', (x + 2800, -400))
     link(_value_socket(read_prof, is_output=True), map_v.inputs['Value'])
     map_v.inputs['From Min'].default_value = 0.0
     map_v.inputs['From Max'].default_value = 1.0
     link(group_in.outputs['V Min'], map_v.inputs['To Min'])
     link(group_in.outputs['V Max'], map_v.inputs['To Max'])
 
-    combine = _add_node(tree, 'ShaderNodeCombineXYZ', (x + 2000, -300))
+    combine = _add_node(tree, 'ShaderNodeCombineXYZ', (x + 3000, -300))
     link(map_u.outputs['Result'], combine.inputs['X'])
     link(map_v.outputs['Result'], combine.inputs['Y'])
 
     # Store as UVMap corner attribute
-    store_uv = _add_node(tree, 'GeometryNodeStoreNamedAttribute', (x + 2200, 0))
+    store_uv = _add_node(tree, 'GeometryNodeStoreNamedAttribute', (x + 3200, 0))
     store_uv.data_type = 'FLOAT2'
     store_uv.domain = 'CORNER'
     store_uv.inputs['Name'].default_value = "UVMap"
@@ -228,21 +272,21 @@ def _build_core(tree, curves_socket, group_in, group_out, x_off=0):
     link(combine.outputs['Vector'], _value_socket(store_uv))
 
     # --- Remove temporary attributes ---
-    rm_path = _add_node(tree, 'GeometryNodeRemoveAttribute', (x + 2400, 0))
+    rm_path = _add_node(tree, 'GeometryNodeRemoveAttribute', (x + 3400, 0))
     rm_path.inputs['Name'].default_value = "_bt_path_factor"
     link(store_uv.outputs['Geometry'], rm_path.inputs['Geometry'])
 
-    rm_prof = _add_node(tree, 'GeometryNodeRemoveAttribute', (x + 2600, 0))
+    rm_prof = _add_node(tree, 'GeometryNodeRemoveAttribute', (x + 3600, 0))
     rm_prof.inputs['Name'].default_value = "_bt_profile_factor"
     link(rm_path.outputs['Geometry'], rm_prof.inputs['Geometry'])
 
     # --- Set material ---
-    set_mat = _add_node(tree, 'GeometryNodeSetMaterial', (x + 2800, 0))
+    set_mat = _add_node(tree, 'GeometryNodeSetMaterial', (x + 3800, 0))
     link(rm_prof.outputs['Geometry'], set_mat.inputs['Geometry'])
     link(group_in.outputs['Material'], set_mat.inputs['Material'])
 
     # --- Output ---
-    group_out.location = (x + 3000, 0)
+    group_out.location = (x + 4000, 0)
     link(set_mat.outputs['Geometry'], group_out.inputs['Geometry'])
 
 
@@ -281,7 +325,13 @@ def _get_or_create_group(name, builder):
             if hasattr(item, 'in_out')
         }
         if {'Geometry', 'Surface Target', 'Material'}.issubset(socket_names):
-            return ng
+            # Also check if the node setup has the cross product (for proper surface alignment)
+            has_cross_product = any(
+                hasattr(n, 'operation') and n.operation == 'CROSS_PRODUCT'
+                for n in ng.nodes
+            )
+            if has_cross_product:
+                return ng
         # Rebuild outdated group
         ng.interface.clear()
         builder(ng)
