967dc792f85d6bfaafd6f13373ba667926c7d87f
tonis2 <tonis.anton18@gmail.com>
2026-03-31 23:50:34 +0300

    Trim path updates

--- trimsheet/trim_path.py
+++ trimsheet/trim_path.py
@@ -35,7 +35,13 @@ def _add_node(tree, bl_idname, location=(0, 0)):
 
 
 def _setup_interface(tree):
-    """Add modifier input sockets to the node group interface."""
+    """Add modifier input/output sockets to the node group interface."""
+    # Geometry I/O (required for geometry nodes modifier)
+    tree.interface.new_socket('Geometry', in_out='INPUT',
+                              socket_type='NodeSocketGeometry')
+    tree.interface.new_socket('Geometry', in_out='OUTPUT',
+                              socket_type='NodeSocketGeometry')
+
     res = tree.interface.new_socket('Resolution', in_out='INPUT',
                                     socket_type='NodeSocketInt')
     res.default_value = 64
@@ -63,6 +69,16 @@ def _setup_interface(tree):
                                       socket_type='NodeSocketFloat')
     v_max.default_value = 1.0
 
+    tree.interface.new_socket('Surface Target', in_out='INPUT',
+                              socket_type='NodeSocketObject')
+
+    snap = tree.interface.new_socket('Snap to Surface', in_out='INPUT',
+                                     socket_type='NodeSocketBool')
+    snap.default_value = True
+
+    tree.interface.new_socket('Material', in_out='INPUT',
+                              socket_type='NodeSocketMaterial')
+
 
 def _set_modifier_input(mod, name, value):
     """Set a geometry nodes modifier input by socket name."""
@@ -74,46 +90,96 @@ def _set_modifier_input(mod, name, value):
 
 
 def _build_core(tree, curves_socket, group_in, group_out, x_off=0):
-    """Build the shared pipeline: curves → mesh with UVs and offset."""
+    """Build the shared pipeline: curves → snap to surface → mesh with UVs."""
     link = tree.links.new
     x = x_off
 
     # --- Resample curve ---
     resample = _add_node(tree, 'GeometryNodeResampleCurve', (x, 0))
-    resample.mode = 'COUNT'
+    if hasattr(resample, 'mode'):
+        resample.mode = 'COUNT'
+    else:
+        resample.inputs['Mode'].default_value = 'Count'
     link(curves_socket, resample.inputs['Curve'])
     link(group_in.outputs['Resolution'], resample.inputs['Count'])
 
-    # --- Store path factor ---
-    sp_path = _add_node(tree, 'GeometryNodeSplineParameter', (x + 200, -200))
+    # --- Snap curve points to surface (before curve-to-mesh) ---
+    obj_info = _add_node(tree, 'GeometryNodeObjectInfo', (x + 200, -500))
+    obj_info.transform_space = 'RELATIVE'
+    link(group_in.outputs['Surface Target'], obj_info.inputs['Object'])
 
-    store_path = _add_node(tree, 'GeometryNodeStoreNamedAttribute', (x + 200, 0))
+    pos_curve = _add_node(tree, 'GeometryNodeInputPosition', (x + 200, -200))
+
+    # Sample nearest position on target surface
+    sample_pos = _add_node(tree, 'GeometryNodeSampleNearestSurface', (x + 400, -200))
+    sample_pos.data_type = 'FLOAT_VECTOR'
+    link(obj_info.outputs['Geometry'], sample_pos.inputs['Mesh'])
+    link(pos_curve.outputs['Position'], sample_pos.inputs['Sample Position'])
+    pos_tgt = _add_node(tree, 'GeometryNodeInputPosition', (x + 400, -350))
+    link(pos_tgt.outputs['Position'], _value_socket(sample_pos))
+
+    # Sample surface normal for offset
+    sample_nrm = _add_node(tree, 'GeometryNodeSampleNearestSurface', (x + 400, -500))
+    sample_nrm.data_type = 'FLOAT_VECTOR'
+    link(obj_info.outputs['Geometry'], sample_nrm.inputs['Mesh'])
+    link(pos_curve.outputs['Position'], sample_nrm.inputs['Sample Position'])
+    nrm_tgt = _add_node(tree, 'GeometryNodeInputNormal', (x + 400, -650))
+    link(nrm_tgt.outputs['Normal'], _value_socket(sample_nrm))
+
+    # Switch: snap or keep original curve positions
+    switch_pos = _add_node(tree, 'GeometryNodeSwitch', (x + 600, -200))
+    switch_pos.input_type = 'VECTOR'
+    link(group_in.outputs['Snap to Surface'], switch_pos.inputs['Switch'])
+    link(pos_curve.outputs['Position'], switch_pos.inputs['False'])
+    link(_value_socket(sample_pos, is_output=True), switch_pos.inputs['True'])
+
+    # Offset snapped position along surface normal
+    nrm_scale = _add_node(tree, 'ShaderNodeVectorMath', (x + 600, -400))
+    nrm_scale.operation = 'SCALE'
+    link(_value_socket(sample_nrm, is_output=True), nrm_scale.inputs[0])
+    link(group_in.outputs['Surface Offset'], nrm_scale.inputs['Scale'])
+
+    # Apply snap + offset to curve points
+    set_curve_pos = _add_node(tree, 'GeometryNodeSetPosition', (x + 800, 0))
+    link(resample.outputs['Curve'], set_curve_pos.inputs['Geometry'])
+    link(switch_pos.outputs['Output'], set_curve_pos.inputs['Position'])
+    link(nrm_scale.outputs['Vector'], set_curve_pos.inputs['Offset'])
+
+    # Set curve normal to surface normal so profile lies flat on surface
+    set_curve_nrm = _add_node(tree, 'GeometryNodeSetCurveNormal', (x + 1000, 0))
+    set_curve_nrm.inputs['Mode'].default_value = 'Minimum Twist'
+    link(set_curve_pos.outputs['Geometry'], set_curve_nrm.inputs['Curve'])
+
+    # --- Store path factor ---
+    sp_path = _add_node(tree, 'GeometryNodeSplineParameter', (x + 1000, -200))
+
+    store_path = _add_node(tree, 'GeometryNodeStoreNamedAttribute', (x + 1200, 0))
     store_path.data_type = 'FLOAT'
     store_path.domain = 'POINT'
     store_path.inputs['Name'].default_value = "_bt_path_factor"
-    link(resample.outputs['Curve'], store_path.inputs['Geometry'])
+    link(set_curve_nrm.outputs['Curve'], store_path.inputs['Geometry'])
     link(sp_path.outputs['Factor'], _value_socket(store_path))
 
     # --- Profile line scaled by Width ---
-    line = _add_node(tree, 'GeometryNodeCurvePrimitiveLine', (x + 200, -500))
+    line = _add_node(tree, 'GeometryNodeCurvePrimitiveLine', (x + 1000, -500))
     line.mode = 'POINTS'
-    line.inputs['Start'].default_value = (0.0, -0.5, 0.0)
-    line.inputs['End'].default_value = (0.0, 0.5, 0.0)
+    line.inputs['Start'].default_value = (-0.5, 0.0, 0.0)
+    line.inputs['End'].default_value = (0.5, 0.0, 0.0)
 
-    pos_in = _add_node(tree, 'GeometryNodeInputPosition', (x + 200, -700))
-    vec_scale_w = _add_node(tree, 'ShaderNodeVectorMath', (x + 400, -700))
+    pos_in = _add_node(tree, 'GeometryNodeInputPosition', (x + 1000, -700))
+    vec_scale_w = _add_node(tree, 'ShaderNodeVectorMath', (x + 1200, -700))
     vec_scale_w.operation = 'SCALE'
     link(pos_in.outputs['Position'], vec_scale_w.inputs[0])
     link(group_in.outputs['Width'], vec_scale_w.inputs['Scale'])
 
-    set_pos_prof = _add_node(tree, 'GeometryNodeSetPosition', (x + 400, -500))
+    set_pos_prof = _add_node(tree, 'GeometryNodeSetPosition', (x + 1200, -500))
     link(line.outputs['Curve'], set_pos_prof.inputs['Geometry'])
     link(vec_scale_w.outputs['Vector'], set_pos_prof.inputs['Position'])
 
     # --- Store profile factor ---
-    sp_prof = _add_node(tree, 'GeometryNodeSplineParameter', (x + 600, -700))
+    sp_prof = _add_node(tree, 'GeometryNodeSplineParameter', (x + 1400, -700))
 
-    store_prof = _add_node(tree, 'GeometryNodeStoreNamedAttribute', (x + 600, -500))
+    store_prof = _add_node(tree, 'GeometryNodeStoreNamedAttribute', (x + 1400, -500))
     store_prof.data_type = 'FLOAT'
     store_prof.domain = 'POINT'
     store_prof.inputs['Name'].default_value = "_bt_profile_factor"
@@ -121,69 +187,63 @@ def _build_core(tree, curves_socket, group_in, group_out, x_off=0):
     link(sp_prof.outputs['Factor'], _value_socket(store_prof))
 
     # --- Curve to Mesh ---
-    c2m = _add_node(tree, 'GeometryNodeCurveToMesh', (x + 800, 0))
+    c2m = _add_node(tree, 'GeometryNodeCurveToMesh', (x + 1600, 0))
     c2m.inputs['Fill Caps'].default_value = False
     link(store_path.outputs['Geometry'], c2m.inputs['Curve'])
     link(store_prof.outputs['Geometry'], c2m.inputs['Profile Curve'])
 
     # --- UV mapping: read factors, map to trim bounds ---
-    read_path = _add_node(tree, 'GeometryNodeInputNamedAttribute', (x + 800, -200))
+    read_path = _add_node(tree, 'GeometryNodeInputNamedAttribute', (x + 1600, -200))
     read_path.data_type = 'FLOAT'
     read_path.inputs['Name'].default_value = "_bt_path_factor"
 
-    map_u = _add_node(tree, 'ShaderNodeMapRange', (x + 1000, -200))
+    map_u = _add_node(tree, 'ShaderNodeMapRange', (x + 1800, -200))
     link(_value_socket(read_path, is_output=True), map_u.inputs['Value'])
     map_u.inputs['From Min'].default_value = 0.0
     map_u.inputs['From Max'].default_value = 1.0
     link(group_in.outputs['U Min'], map_u.inputs['To Min'])
     link(group_in.outputs['U Max'], map_u.inputs['To Max'])
 
-    read_prof = _add_node(tree, 'GeometryNodeInputNamedAttribute', (x + 800, -400))
+    read_prof = _add_node(tree, 'GeometryNodeInputNamedAttribute', (x + 1600, -400))
     read_prof.data_type = 'FLOAT'
     read_prof.inputs['Name'].default_value = "_bt_profile_factor"
 
-    map_v = _add_node(tree, 'ShaderNodeMapRange', (x + 1000, -400))
+    map_v = _add_node(tree, 'ShaderNodeMapRange', (x + 1800, -400))
     link(_value_socket(read_prof, is_output=True), map_v.inputs['Value'])
     map_v.inputs['From Min'].default_value = 0.0
     map_v.inputs['From Max'].default_value = 1.0
     link(group_in.outputs['V Min'], map_v.inputs['To Min'])
     link(group_in.outputs['V Max'], map_v.inputs['To Max'])
 
-    combine = _add_node(tree, 'ShaderNodeCombineXYZ', (x + 1200, -300))
+    combine = _add_node(tree, 'ShaderNodeCombineXYZ', (x + 2000, -300))
     link(map_u.outputs['Result'], combine.inputs['X'])
     link(map_v.outputs['Result'], combine.inputs['Y'])
 
     # Store as UVMap corner attribute
-    store_uv = _add_node(tree, 'GeometryNodeStoreNamedAttribute', (x + 1400, 0))
-    store_uv.data_type = 'FLOAT_VECTOR'
+    store_uv = _add_node(tree, 'GeometryNodeStoreNamedAttribute', (x + 2200, 0))
+    store_uv.data_type = 'FLOAT2'
     store_uv.domain = 'CORNER'
     store_uv.inputs['Name'].default_value = "UVMap"
     link(c2m.outputs['Mesh'], store_uv.inputs['Geometry'])
     link(combine.outputs['Vector'], _value_socket(store_uv))
 
     # --- Remove temporary attributes ---
-    rm_path = _add_node(tree, 'GeometryNodeRemoveAttribute', (x + 1600, 0))
+    rm_path = _add_node(tree, 'GeometryNodeRemoveAttribute', (x + 2400, 0))
     rm_path.inputs['Name'].default_value = "_bt_path_factor"
     link(store_uv.outputs['Geometry'], rm_path.inputs['Geometry'])
 
-    rm_prof = _add_node(tree, 'GeometryNodeRemoveAttribute', (x + 1800, 0))
+    rm_prof = _add_node(tree, 'GeometryNodeRemoveAttribute', (x + 2600, 0))
     rm_prof.inputs['Name'].default_value = "_bt_profile_factor"
     link(rm_path.outputs['Geometry'], rm_prof.inputs['Geometry'])
 
-    # --- Surface offset along normal ---
-    normal = _add_node(tree, 'GeometryNodeInputNormal', (x + 1800, -200))
-    vec_scale_n = _add_node(tree, 'ShaderNodeVectorMath', (x + 2000, -200))
-    vec_scale_n.operation = 'SCALE'
-    link(normal.outputs['Normal'], vec_scale_n.inputs[0])
-    link(group_in.outputs['Surface Offset'], vec_scale_n.inputs['Scale'])
-
-    set_pos_off = _add_node(tree, 'GeometryNodeSetPosition', (x + 2200, 0))
-    link(rm_prof.outputs['Geometry'], set_pos_off.inputs['Geometry'])
-    link(vec_scale_n.outputs['Vector'], set_pos_off.inputs['Offset'])
+    # --- Set material ---
+    set_mat = _add_node(tree, 'GeometryNodeSetMaterial', (x + 2800, 0))
+    link(rm_prof.outputs['Geometry'], set_mat.inputs['Geometry'])
+    link(group_in.outputs['Material'], set_mat.inputs['Material'])
 
     # --- Output ---
-    group_out.location = (x + 2400, 0)
-    link(set_pos_off.outputs['Geometry'], group_out.inputs['Geometry'])
+    group_out.location = (x + 3000, 0)
+    link(set_mat.outputs['Geometry'], group_out.inputs['Geometry'])
 
 
 def _build_gp_group(tree):
@@ -215,10 +275,60 @@ def _get_or_create_group(name, builder):
     """Return an existing node group or create a new one."""
     ng = bpy.data.node_groups.get(name)
     if ng is not None:
+        # Validate that the group has required sockets
+        socket_names = {
+            item.name for item in ng.interface.items_tree
+            if hasattr(item, 'in_out')
+        }
+        if {'Geometry', 'Surface Target', 'Material'}.issubset(socket_names):
+            return ng
+        # Rebuild outdated group
+        ng.interface.clear()
+        builder(ng)
         return ng
     ng = bpy.data.node_groups.new(name, 'GeometryNodeTree')
     builder(ng)
     return ng
+
+
+def _get_or_create_trim_material(trim_settings):
+    """Find or create a material using the active trimsheet's image."""
+    ts = trim_settings.get_active_trimsheet()
+    if ts is None:
+        return None
+
+    ts_name = ts.name
+    mat_name = f"Trim_{ts_name}"
+
+    # Return existing material if already created
+    mat = bpy.data.materials.get(mat_name)
+    if mat is not None:
+        return mat
+
+    # Search for an image matching the trimsheet name
+    trim_img = None
+    ts_lower = ts_name.lower().replace(' ', '_')
+    for img in bpy.data.images:
+        img_base = img.name.rsplit('.', 1)[0] if '.' in img.name else img.name
+        if img_base.lower().replace(' ', '_') == ts_lower:
+            trim_img = img
+            break
+
+    if trim_img is None:
+        return None
+
+    # Create material with the trim sheet image
+    mat = bpy.data.materials.new(name=mat_name)
+    mat.use_nodes = True
+    tree = mat.node_tree
+    principled = tree.nodes.get('Principled BSDF')
+
+    tex_node = tree.nodes.new('ShaderNodeTexImage')
+    tex_node.image = trim_img
+    tex_node.location = (-300, 300)
+    tree.links.new(tex_node.outputs['Color'], principled.inputs['Base Color'])
+
+    return mat
 
 
 def _get_region_uv_bounds(region):
@@ -277,16 +387,20 @@ class BT_OT_ConvertToTrimPath(Operator):
         _set_modifier_input(mod, 'V Min', v_min)
         _set_modifier_input(mod, 'V Max', v_max)
 
-        # Auto-assign material from a selected mesh
-        mat = None
+        # Auto-assign surface target from a selected mesh
+        target_mesh = None
         for sel_obj in context.selected_objects:
-            if sel_obj.type == 'MESH' and sel_obj != obj and sel_obj.data.materials:
-                mat = sel_obj.data.materials[0]
+            if sel_obj.type == 'MESH' and sel_obj != obj:
+                target_mesh = sel_obj
                 break
 
-        if mat is not None:
-            if mat.name not in [m.name for m in obj.data.materials if m]:
-                obj.data.materials.append(mat)
+        if target_mesh is not None:
+            _set_modifier_input(mod, 'Surface Target', target_mesh)
+
+        # Create/find a material with the trim sheet texture
+        trim_mat = _get_or_create_trim_material(trim)
+        if trim_mat is not None:
+            _set_modifier_input(mod, 'Material', trim_mat)
 
         # Switch Properties editor to modifier tab
         for area in context.screen.areas:
